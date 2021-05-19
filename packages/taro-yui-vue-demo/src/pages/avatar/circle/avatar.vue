79a85d988e2c0ff61ff2d7ee8d804890151d6c3a
liumingming <liumingming@yxml.net>
2021-05-19 11:48:07 +0800

    修正用户图像404 bug

--- packages/taro-yui-vue-demo/src/pages/avatar/circle/avatar.vue
+++ packages/taro-yui-vue-demo/src/pages/avatar/circle/avatar.vue
@@ -1,18 +1,19 @@
 <template>
   <view v-reSize id="container" class="container">
-    <y-avatar :src="imgSrc" :isCircle="true" :size="180"></y-avatar>
+    <y-avatar :src="defaultAvatar" :isCircle="true" :size="180"></y-avatar>
   </view>
 </template>
 <script lang="ts">
 import { defineComponent, defineAsyncComponent } from 'vue'
 import './avatar.scss'
+import defaultAvatar from '../../../assets/img/default_avatar.jpg'
 export default defineComponent({
   components: {
     'y-avatar': defineAsyncComponent(() => import('@/components/yui/avatar/avatar.vue'))
   },
   setup() {
     return {
-      imgSrc: 'https://res.xsrkj.cn/upload/20210108/0bbbfeb24fba8ec3e3f4aa99cc25050b.jpg'
+      defaultAvatar
     };
   }
 });
